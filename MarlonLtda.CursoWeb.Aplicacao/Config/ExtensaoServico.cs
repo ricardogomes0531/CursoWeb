283c097b5092fca8b0c9221b571945373776ddfc
ricardo gomes <ej.ricardogomes@gmail.com>
2021-05-24 19:40:06 -0300

    Arquitetura inicial em 3 camadas.

---
+++ MarlonLtda.CursoWeb.Aplicacao/Config/ExtensaoServico.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarlonLtda.CursoWeb.Aplicacao.Config
+{
+public static    class ExtensaoServico
+    {
+        public static IServiceCollection AdicionarServico(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
---
+++ MarlonLtda.CursoWeb.Infra/Config/ExtensaoInfra.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarlonLtda.CursoWeb.Infra.Config
+{
+     public static class ExtensaoInfra
+    {
+        public static IServiceCollection AdicionarDependenciaInfra(this IServiceCollection servico)
+        {
+            return servico;
+        }
+    }
+}
